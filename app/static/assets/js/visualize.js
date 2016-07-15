f1b177b4c7ba0d4786f57fb6a0cbf5577a20ec27
iRB Air <iROCKBUNNY@users.noreply.github.com>
2016-07-16 04:23:56 +0800

    Update

--- app/static/assets/js/visualize.js
+++ app/static/assets/js/visualize.js
@@ -1,1 +1,43 @@
 // visualize.js
+
+$(document)
+  .ready(function() {
+
+    // fix menu when passed
+    $('.masthead')
+      .visibility({
+        once: false,
+        onBottomPassed: function() {
+          $('.fixed.menu').transition('fade in');
+        },
+        onBottomPassedReverse: function() {
+          $('.fixed.menu').transition('fade out');
+        }
+      })
+    ;
+
+    // create sidebar and attach to menu open
+    $('.ui.sidebar')
+      .sidebar('attach events', '.toc.item')
+    ;
+
+  })
+;
+
+// popup wechat qr code
+$('#wechat-qr-code-click')
+  .popup({
+    popup : $('#wechat-qr-code'),
+    on    : 'click'
+  })
+;
+
+// copyright
+$('#copyright')
+  .html(
+   function(){
+     var date = new Date();
+     return '&copy; 2011-' +  date.getFullYear() + ' 北京云英一语教育咨询有限公司 版权所有';
+   }
+  )
+;
