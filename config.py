268713cba997d902dc5a817f6de8a93d926793f8
iRB Air <iROCKBUNNY@users.noreply.github.com>
2016-07-16 15:26:31 +0800

    Update

--- config.py
+++ config.py
@@ -27,12 +27,35 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'ysys-beta.sqlite')
+        'sqlite:///' + os.path.join(basedir, 'ysys-dev.sqlite')
 
 
 class BetaConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'ysys-beta.sqlite')
+
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+
+        # email errors to the administrators
+        import logging
+        from logging.handlers import SMTPHandler
+        credentials = None
+        secure = None
+        if getattr(cls, 'MAIL_USERNAME', None) is not None:
+            credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+            if getattr(cls, 'MAIL_USE_SSL', None):
+                secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
+            fromaddr=cls.YSYS_MAIL_SENDER,
+            toaddrs=[cls.YSYS_ADMIN],
+            subject=cls.YSYS_MAIL_SUBJECT_PREFIX + ' Application Error',
+            credentials=credentials,
+            secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
 
 
 class ProductionConfig(Config):
