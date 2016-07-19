7cb8cb8369d8d10bcfa77ead5dec58ce6dc90744
iRB Air <iROCKBUNNY@users.noreply.github.com>
2016-07-19 10:05:45 +0800

    Update

--- app/models.py
+++ app/models.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import sys
-
-reload(sys)
-sys.setdefaultencoding('utf-8');
-
 from datetime import datetime
 import hashlib
 from werkzeug.security import generate_password_hash, check_password_hash
