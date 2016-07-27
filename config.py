eef739436b107dc6f2128e392366cc6d379682e5
iRB Air <iROCKBUNNY@users.noreply.github.com>
2016-07-27 11:37:22 +0800

    Update

--- config.py
+++ config.py
@@ -40,28 +40,28 @@ class BetaConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'ysys-beta.sqlite')
 
-    @classmethod
-    def init_app(cls, app):
-        Config.init_app(app)
+    # @classmethod
+    # def init_app(cls, app):
+    #     Config.init_app(app)
 
-        # email errors to the administrators
-        import logging
-        from logging.handlers import SMTPHandler
-        credentials = None
-        secure = None
-        if getattr(cls, 'MAIL_USERNAME', None) is not None:
-            credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
-            if getattr(cls, 'MAIL_USE_SSL', None):
-                secure = ()
-        mail_handler = SMTPHandler(
-            mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
-            fromaddr=cls.YSYS_MAIL_SENDER,
-            toaddrs=[cls.YSYS_ADMIN],
-            subject=cls.YSYS_MAIL_SUBJECT_PREFIX + ' Application Error',
-            credentials=credentials,
-            secure=secure)
-        mail_handler.setLevel(logging.ERROR)
-        app.logger.addHandler(mail_handler)
+    #     # email errors to the administrators
+    #     import logging
+    #     from logging.handlers import SMTPHandler
+    #     credentials = None
+    #     secure = None
+    #     if getattr(cls, 'MAIL_USERNAME', None) is not None:
+    #         credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+    #         if getattr(cls, 'MAIL_USE_SSL', None):
+    #             secure = ()
+    #     mail_handler = SMTPHandler(
+    #         mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
+    #         fromaddr=cls.YSYS_MAIL_SENDER,
+    #         toaddrs=[cls.YSYS_ADMIN],
+    #         subject=cls.YSYS_MAIL_SUBJECT_PREFIX + ' Application Error',
+    #         credentials=credentials,
+    #         secure=secure)
+    #     mail_handler.setLevel(logging.ERROR)
+    #     app.logger.addHandler(mail_handler)
 
 
 class ProductionConfig(Config):
