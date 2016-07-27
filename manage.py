b6cecdb6d4377642a2cd78c3928e489b76b6e282
iRB Air <iROCKBUNNY@users.noreply.github.com>
2016-07-27 11:23:24 +0800

    Update

--- manage.py
+++ manage.py
@@ -25,7 +25,7 @@ migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role, Punch=Punch)
+    return dict(app=app, db=db)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
