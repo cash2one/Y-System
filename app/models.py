9dd7db4008d6f7e9158908f248e04e797cdc91f1
iRB Air <iROCKBUNNY@users.noreply.github.com>
2016-11-23 06:05:17 +0800

    Update

--- app/models.py
+++ app/models.py
@@ -487,11 +487,12 @@ class UserAnnouncement(db.Model):
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.Unicode(64), unique=True, index=True)
     name = db.Column(db.Unicode(64), index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
+    email = db.Column(db.Unicode(64), unique=True, index=True)
     confirmed = db.Column(db.Boolean, default=False)
+    # activated = db.Column(db.Boolean, default=False)
     member_since = db.Column(db.DateTime, default=datetime.utcnow)
     last_seen_at = db.Column(db.DateTime, default=datetime.utcnow)
     deleted = db.Column(db.Boolean, default=False)
