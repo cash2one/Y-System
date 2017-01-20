df2713b84958958887b840ed8efdb43a43303be9
iRB Air <iROCKBUNNY@users.noreply.github.com>
2017-01-20 14:19:00 +0000

    Update

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from flask import Blueprint
+from flask import Blueprint, current_app
 
 main = Blueprint('main', __name__)
 
 from . import views, errors
+
+
+@main.app_context_processor
+def inject_version():
+    return dict(Version=current_app.config['VERSION'])
