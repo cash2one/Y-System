59bf993aeb659ab68292716f76931fe6447b32f7
iRB Air <iROCKBUNNY@users.noreply.github.com>
2017-01-23 00:10:59 +0000

    Update

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from datetime import date
-from flask import render_template, redirect, url_for, abort, flash, request, current_app, make_response
+from flask import render_template, redirect, url_for, abort, flash, request, current_app
 from flask_login import login_required, current_user
 from flask_sqlalchemy import get_debug_queries
 from . import main
