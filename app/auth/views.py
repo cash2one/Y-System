d7c2da538c7313d614fbed391c176b6df09c2b4f
iRB Air <iROCKBUNNY@users.noreply.github.com>
2017-02-21 21:03:06 +0800

    Update

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from datetime import datetime, timedelta
 from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
